9c88e49f5ddf792f118022210c63df796a64b465
Kelly Doyle <>
2018-02-26 19:02:05 -0500

    Updates to record transactions

--- pharma-provenance-blockchain/data/data_loader.sh
+++ pharma-provenance-blockchain/data/data_loader.sh
@@ -12,6 +12,18 @@ curl -X POST --header 'Content-Type: application/json' --header 'Accept: applica
    "companyName": "BigThree Meds Distribution"  
  }' 'http://localhost:3000/api/SupplyChainPartner'
 
+ curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{  
+   "$class": "org.e599.model.SupplyChainPartner",  
+   "partnerId": "3",  
+   "companyName": "Drugs R Us"  
+ }' 'http://localhost:3000/api/SupplyChainPartner'
+
+  curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{  
+   "$class": "org.e599.model.SupplyChainPartner",  
+   "partnerId": "4",  
+   "companyName": "Pharma Repackaging Ltd"  
+ }' 'http://localhost:3000/api/SupplyChainPartner'
+
  # Add products
  curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{  
    "$class": "org.e599.model.Product",  
@@ -26,12 +38,6 @@ curl -X POST --header 'Content-Type: application/json' --header 'Accept: applica
    "productName": "Flemendrex",  
    "manufacturer": "org.e599.model.SupplyChainPartner#1"  
  }' 'http://localhost:3000/api/Product'
- 
- curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{
-  "$class": "org.e599.model.IndividualPackage",
-  "gtin": "gtin1111",
-  "product": "org.e599.model.Product#1"
-}' 'http://localhost:3000/api/IndividualPackage'
 
 # Locations
 curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{
@@ -48,6 +54,68 @@ curl -X POST --header 'Content-Type: application/json' --header 'Accept: applica
   "company": "org.e599.model.SupplyChainPartner#2" 
 }' 'http://localhost:3000/api/Location'
 
+curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{
+  "$class": "org.e599.model.Location",
+  "gln": "gln4444",
+  "address": "550 Madison ave. New York, NY 10002",
+  "company": "org.e599.model.SupplyChainPartner#3"
+}' 'http://localhost:3000/api/Location'
+
+curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{
+  "$class": "org.e599.model.Location",
+  "gln": "gln1111",
+  "address": "13 Flats Rd. Salt Lake City, UT 84108",
+  "company": "org.e599.model.SupplyChainPartner#4" 
+}' 'http://localhost:3000/api/Location'
+
+# Batch
+curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{
+  "$class" : "org.e599.model.Batch",
+  "batchId" : "B0001",
+  "product" : "org.e599.model.Product#1",
+  "batchCompletedDate" : "2018-01-02T11:42Z",
+  "manufactureLocation" : "org.e599.model.Location#gln2222"
+}' 'http://localhost:3000/api/Batch'
+
+curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{
+  "$class" : "org.e599.model.Batch",
+  "batchId" : "B0002",
+  "product" : "org.e599.model.Product#1",
+  "batchCompletedDate" : "2018-02-02T11:42Z",
+  "manufactureLocation" : "org.e599.model.Location#gln2222"
+}' 'http://localhost:3000/api/Batch'
+
+# Individual package
+ curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{
+  "$class": "org.e599.model.IndividualPackage",
+  "gtin": "gtin1111",
+  "product": "org.e599.model.Product#1",
+  "unitCount" : 500,
+  "unit" : "mg",
+  "dosage" : 25,
+  "batch" : "org.e599.model.Batch#B0001"
+}' 'http://localhost:3000/api/IndividualPackage'
+
+ curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{
+  "$class": "org.e599.model.IndividualPackage",
+  "gtin": "gtin1112",
+  "product": "org.e599.model.Product#1",
+  "unitCount" : 500,
+  "unit" : "mg",
+  "dosage" : 25,
+  "batch" : "org.e599.model.Batch#B0001"
+}' 'http://localhost:3000/api/IndividualPackage'
+
+ curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{
+  "$class": "org.e599.model.IndividualPackage",
+  "gtin": "gtin1113",
+  "product": "org.e599.model.Product#1",
+  "unitCount" : 500,
+  "unit" : "mg",
+  "dosage" : 25,
+  "batch" : "org.e599.model.Batch#B0001"
+}' 'http://localhost:3000/api/IndividualPackage'
+
 # Containers
 curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{
   "$class": "org.e599.model.Container",
@@ -56,6 +124,13 @@ curl -X POST --header 'Content-Type: application/json' --header 'Accept: applica
   "packages": [
     "org.e599.model.IndividualPackage#gtin1111"
   ]
+}' 'http://localhost:3000/api/Container'
+
+curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{
+  "$class": "org.e599.model.Container",
+  "gtin": "gtin-cont-2",
+  "currentLocation": "org.e599.model.Location#gln2222",
+  "packages": []
 }' 'http://localhost:3000/api/Container'
 
 # Define shipments
@@ -67,10 +142,29 @@ curl -X POST --header 'Content-Type: application/json' --header 'Accept: applica
   "sendTo": "org.e599.model.Location#gln3333"
 }' 'http://localhost:3000/api/Shipment'
 
-# Execute a delivery transaction.
 curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{
-  "$class": "org.e599.model.Delivery",
+  "$class": "org.e599.model.Shipment",
+  "sscc": "SSCC-1112",
+  "container": "org.e599.model.Container#gtin-cont-1",
+  "sendFrom": "org.e599.model.Location#gln3333",
+  "sendTo": "org.e599.model.Location#gln4444"
+}' 'http://localhost:3000/api/Shipment'
+
+
+# Execute packaging transactions
+curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{
+  "$class" : "org.e599.model.PackageTransaction",
+  "container" : "org.e599.model.Container#gtin-cont-2",
+  "contents" : ["org.e599.model.IndividualPackage#gtin1112", "org.e599.model.IndividualPackage#gtin1113"]
+}' 'http://localhost:3000/api/PackageTransaction'
+
+# Execute delivery transactions.
+curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{
+  "$class": "org.e599.model.DeliveryTransaction",
   "shipment": "org.e599.model.Shipment#SSCC-1111"
-}' 'http://localhost:3000/api/Delivery'
+}' 'http://localhost:3000/api/DeliveryTransaction'
 
-
+curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{
+  "$class": "org.e599.model.DeliveryTransaction",
+  "shipment": "org.e599.model.Shipment#SSCC-1112"
+}' 'http://localhost:3000/api/DeliveryTransaction'
--- pharma-provenance-blockchain/lib/script.js
+++ pharma-provenance-blockchain/lib/script.js
@@ -1,10 +1,40 @@
+
 /**
  * Track the delivery of a package from one partner to another
- * @param {org.e599.model.Delivery} delivery - the delivery to be processed
+ * @param {org.e599.model.PackageTransaction} packageTransaction - the package order
+ * @transaction
+ */
+function packageContainer(packageTransaction) {
+    packageTransaction.container.packages = packageTransaction.contents;
+    // TODO: loop through individual packages and set current container.
+
+    return getAssetRegistry('org.e599.model.Container')
+    .then(function (assetRegistry) {
+        return assetRegistry.update(packageTransaction.container);
+    });
+
+    //TODO: Log that individual items were added to a container
+}
+
+/**
+ * Track the delivery of a package from one partner to another
+ * @param {org.e599.model.DeliveryTransaction} delivery - the delivery to be processed
  * @transaction
  */
 function receivePackage(delivery) {
+    var factory = getFactory();
+
     delivery.shipment.container.currentLocation = delivery.shipment.sendTo;
+    
+    var pod = factory.newResource('org.e599.model', 'ProofOfDelivery', delivery.shipment.sscc);
+    pod.gtin = delivery.shipment.container.gtin;
+    pod.gln = delivery.shipment.sendTo.gln;
+
+    getAssetRegistry('org.e599.model.ProofOfDelivery')
+        .then(function (assetRegistry) {
+            assetRegistry.add(pod);
+        });
+
     return getAssetRegistry('org.e599.model.Container')
         .then(function (assetRegistry) {
             return assetRegistry.update(delivery.shipment.container);
